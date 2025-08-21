6bc6b6c6f1d09c2f147487df98f7cec456c81be6
Harsh Gupta <harshgupta274402@gmail.com>
2025-08-21 14:34:59 +0530

    Server side Route Protection

--- api/routes/Bloglike.route.js
+++ api/routes/Bloglike.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { doLike, likeCount } from '../controllers/BlogLike.controller.js'
-// import { authenticate } from '../middleware/authenticate.js'
+import { authenticate } from '../middleware/authenticate.js'
 
 const BlogLikeRoute = express.Router()
 
@@ -8,7 +8,7 @@ const BlogLikeRoute = express.Router()
 // BlogLikeRoute.get('/get-like/:blogid/:userid?', likeCount)
 
 
-BlogLikeRoute.post('/do-like', doLike)
-BlogLikeRoute.get('/get-like/:blogid/', likeCount)
+BlogLikeRoute.post('/do-like', authenticate, doLike)
+BlogLikeRoute.get('/get-like/:blogid/', authenticate, likeCount)
 
 export default BlogLikeRoute
